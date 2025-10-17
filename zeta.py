a4004e81aa83f31e2479923499e67602557ab47a
velprakash <velprakashs583224148054@nprcolleges.org>
2025-10-17 09:44:55 +0530

    palindrome

--- zeta.py
+++ zeta.py
@@ -1,1 +1,18 @@
+def reverse_list(user_input):
+    reverse = user_input[len(user_input):0:-1]
+    reverse = reverse + user_input[0]
+    return reverse
+def main():
 
+    while True:
+        user_input = input("Enter a word: ")
+        reverse = reverse_list(user_input)
+        if reverse == user_input:
+            print("It's a Palindrome!")
+            print(reverse)
+        else:
+            print("It's not a Palindrome")
+            print(reverse)
+            break
+if __name__ =="__main__":
+    main()
