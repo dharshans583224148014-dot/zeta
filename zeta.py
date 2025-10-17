caa954541a8bb7071beba5e53d344d5ba36425f8
deva <devkishant583224148010@nprcolleges.org>
2025-10-17 09:49:39 +0530

    fibonocci series

--- zeta.py
+++ zeta.py
@@ -1,1 +1,11 @@
-
+def fibonacci_loop(n_terms):
+    if n_terms <= 0:
+        return []
+    elif n_terms == 1:
+        return [0]
+    else:
+        series = [0, 1]
+        while len(series) < n_terms:
+            next_term = series[-1] + series[-2]
+            series.append(next_term)
+        return series
